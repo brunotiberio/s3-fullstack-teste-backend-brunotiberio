23b9661554f182eb7a7997c0afa12f278294c5cb
Bruno Tiberio <quentemente@gmail.com>
2022-11-30 09:16:03 -0300

    feat: routes

---
+++ src/routes/index.routes.ts
@@ -0,0 +1,9 @@
+import { Express } from "express";
+
+const appRoutes = (app: Express) => {
+  //   app.use("/login");
+  //   app.use("/users");
+  //   app.use("/contacts");
+};
+
+export default appRoutes;
