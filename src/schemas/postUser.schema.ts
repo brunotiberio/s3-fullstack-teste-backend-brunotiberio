1dc62e28efb77240c46f9d08bcfd3fe4ebdf5f50
Bruno Tiberio <quentemente@gmail.com>
2022-12-01 19:35:18 -0300

    fix: schema de emails e phones

--- src/schemas/postUser.schema.ts
+++ src/schemas/postUser.schema.ts
@@ -2,10 +2,10 @@ import * as yup from "yup";
 import { SchemaOf } from "yup";
 import { IUserRequest } from "../interfaces/users";
 
-export const postLoginSchema: SchemaOf<IUserRequest> = yup.object().shape({
+export const postUserSchema: SchemaOf<IUserRequest> = yup.object().shape({
   fullname: yup.string().required("'Nome completo é obrigatório'"),
   username: yup.string().required("'Username é obrigatório'"),
   password: yup.string().required("'Senha é obrigatória'"),
-  emails: yup.array().required("'Email é obrigatório'"),
-  phones: yup.array().required("'Telefone é obrigatório'"),
+  emails: yup.array().of(yup.string().email().required("'Email é obrigatório'")).required("'Email é obrigatório'"),
+  phones: yup.array().of(yup.string().required("'Telefone é obrigatório'")).required("'Telefone é obrigatório'"),
 });
