6dc313f010c99701c0c66faad2a67153b35c0650
Bruno Tiberio <quentemente@gmail.com>
2022-12-01 19:35:43 -0300

    feat: rotas de usuario

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,11 @@
 import { Express } from "express";
 
 import loginRouter from "./login.routes";
+import userRouter from "./users.routes";
 
 const appRoutes = (app: Express) => {
   app.use("/login", loginRouter);
-  //   app.use("/users");
+  app.use("/users", userRouter);
   //   app.use("/contacts");
 };
 
